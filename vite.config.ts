cd3d9f3a3c5382414c94c102b1fc7e1615403412
Alex Li <alex.li@aurasemi.com.cn>
2026-06-25 13:53:16 +0800

    Initial deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/delta-force-ttk-s9/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
